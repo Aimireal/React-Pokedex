cce21121a3eff683855409af838e4ef018b6491b
Dylan <dylanhudson1998@gmail.com>
2021-10-29 21:39:06 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# React Pokedex
+This is a quick site to display the original 151 Pokemon written in 
+Javascript using the React framework
+
+## Features:
+- Dark theme
+- Coloured title bars for each element
+- On Click Menu
+    - Type Display
+    - Sprites (Regular/Shiny)
+    - Abilities
+
+## Tech Stack
+Javascript, React, Bootstrap, Axios
+
+![alt_text](https://github.com/Aimireal/React-Pokedex/public/images/pokedex.png)
+
+## To Run:
+(Will be added later, either will make a host website to run these off of or post individually to Vercel/Netlify)
---
+++ src/components/Loader.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Spinner, Col, Row } from 'react-bootstrap'
+
+const Loader = () => {
+    return (
+        <div className='d-flex justify-content-center mt-5' style={{height: '100vh'}}>
+            <Row>
+                <Col>
+                    <Spinner
+                        className='spinner-border spinner-border-lg'
+                        role='status'
+                        style={{ height: '5vh', width: '5vh '}}
+                        variant="light"
+                    >
+                    </Spinner>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <div className='mx-3'>Fetching Pokemon</div>
+                </Col>
+            </Row>
+        </div>
+    )
+}
+
+export default Loader;
